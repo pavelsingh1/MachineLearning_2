f261c9e138e1cd3a363d2070a3f91a1511d2dd83
JojoKn <h12113366@s.wu.ac.at>
2023-01-22 10:48:27 +0100

    Getting started

--- Assignment_2_Script.R
+++ Assignment_2_Script.R
@@ -1,3 +1,17 @@
-#Machine Learning, H" 2022/2023, Assignment 2#
-#Owner: JK#
+#Machine Learning, Assignment 2#
+#Owner: JK, PS#
+
+#Searching for ideas#
+
+#Goal: Predicting financial literacy question answers
+
+#Dataset: National Financial Capability Survey
+library(readr)
+data <- read_csv("2021-SxS-Data-and-Data-Info/NFCS 2021 State Data 220627.csv")
+View(data)
+
+#Data cleaning
+
+
+#Getting Summary Statistics
 
